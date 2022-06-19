755757e3d026c3f66e1977c6ac7716192bae0f40
NejcTi <nejc.tic@gmail.com>
2022-06-19 19:28:24 +0200

    Search recycler basics

--- app/src/main/java/uni_lj/fe/tunv/projekt/toot_orino/MainActivityStudent.java
+++ app/src/main/java/uni_lj/fe/tunv/projekt/toot_orino/MainActivityStudent.java
@@ -6,14 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivityStudent extends AppCompatActivity{
+    private StudentSearchAdapter.RecyclerClickListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,8 @@ public class MainActivityStudent extends AppCompatActivity{
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         recyclerView.setAdapter(adapter);
 
+        setAdapter();
+
         TextView switcher = findViewById(R.id.switch_tag_student);
         switcher.setOnClickListener(v -> {
             startActivity(new Intent(MainActivityStudent.this, MainActivityTutor.class));
@@ -36,5 +41,35 @@ public class MainActivityStudent extends AppCompatActivity{
         menuSwitcher.setOnClickListener(v -> {
             startActivity(new Intent(MainActivityStudent.this, ScheduleActivityStudent.class));
         });
+    }
+
+    private void setAdapter() {
+        setOnClickListener();
+        Timestamp test = new Timestamp(2000, 5, 6, 20, 20, 0, 0);
+        ArrayList<Timeslot> timeslots = new ArrayList<Timeslot>();
+        Timeslot timeslot1 = new Timeslot("00", "10", new Subject("Matematika", 5), "test1Details", "4", test, test, "Janezova ulica 5");
+        Timeslot timeslot2 = new Timeslot("00", "10", new Subject("Biologija", 6), "test1Details", "4", test, test, "Janezova ulica 5");
+        timeslots.add(timeslot1);
+        timeslots.add(timeslot2);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.timeslots_student_recycle_view);
+        StudentSearchAdapter STadapter = new StudentSearchAdapter(timeslots, listener);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(STadapter);
+    }
+    //For expanding box with aditional info
+    private void setOnClickListener() {
+        listener = new StudentSearchAdapter.RecyclerClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                TextView locationView = view.findViewById(R.id.location_text);
+                if(locationView.getVisibility() == View.GONE) {
+                    locationView.setVisibility(View.VISIBLE);
+                }else{
+                    locationView.setVisibility(View.GONE);
+                }
+            }
+        };
     }
 }
--- app/src/main/java/uni_lj/fe/tunv/projekt/toot_orino/StudentSearchAdapter.java
+++ app/src/main/java/uni_lj/fe/tunv/projekt/toot_orino/StudentSearchAdapter.java
@@ -1,22 +1,20 @@
 package uni_lj.fe.tunv.projekt.toot_orino;
 
-import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
-public class TutorTimeslotAdapter extends RecyclerView.Adapter<TutorTimeslotAdapter.ViewHolder> {
+public class StudentSearchAdapter extends RecyclerView.Adapter<StudentSearchAdapter.ViewHolder> {
     private ArrayList<Timeslot> timeslots;
-    private static RecyclerClickListener listener;
+    private static StudentSearchAdapter.RecyclerClickListener listener;
 
-    public TutorTimeslotAdapter(ArrayList<Timeslot> timeslots, RecyclerClickListener listener){
+    public StudentSearchAdapter(ArrayList<Timeslot> timeslots, StudentSearchAdapter.RecyclerClickListener listener){
         this.timeslots = timeslots;
         this.listener = listener;
     }
@@ -24,19 +22,21 @@ public class TutorTimeslotAdapter extends RecyclerView.Adapter<TutorTimeslotAdap
 
     @NonNull
     @Override
-    public TutorTimeslotAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public StudentSearchAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View listItem= layoutInflater.inflate(R.layout.tutor_timeslot_card, parent, false);
-        TutorTimeslotAdapter.ViewHolder viewHolder = new TutorTimeslotAdapter.ViewHolder(listItem);
+        View listItem= layoutInflater.inflate(R.layout.student_timeslot_search_card, parent, false);
+        StudentSearchAdapter.ViewHolder viewHolder = new StudentSearchAdapter.ViewHolder(listItem);
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull StudentSearchAdapter.ViewHolder holder, int position) {
         holder.nameView.setText(timeslots.get(position).getStudentID());
         holder.subjectView.setText(timeslots.get(position).getSubject().getName());
+        holder.hourlyrateView.setText(new StringBuilder().append("$").append(timeslots.get(position).getSubject().getHourlyRate()).append("/h"));
         holder.dateView.setText(new StringBuilder().append(timeslots.get(position).getStartDate().getDate()).append(".").append(timeslots.get(position).getStartDate().getMonth()).toString());
         holder.timestampView.setText(timeslots.get(position).getStartDate().getHours() + ":" + timeslots.get(position).getStartDate().getMinutes());
+        holder.locationView.setText(timeslots.get(position).getLocation());
 
     }
     @Override
@@ -47,6 +47,7 @@ public class TutorTimeslotAdapter extends RecyclerView.Adapter<TutorTimeslotAdap
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public TextView nameView;
         public TextView subjectView;
+        public TextView hourlyrateView;
         public TextView dateView;
         public TextView timestampView;
         public TextView locationView;
@@ -54,8 +55,9 @@ public class TutorTimeslotAdapter extends RecyclerView.Adapter<TutorTimeslotAdap
 
         public ViewHolder(View itemView) {
             super(itemView);
-            this.nameView = (TextView) itemView.findViewById(R.id.studentname_text);
+            this.nameView = (TextView) itemView.findViewById(R.id.tutorname_text);
             this.subjectView = (TextView) itemView.findViewById(R.id.subject_text);
+            this.hourlyrateView = (TextView) itemView.findViewById(R.id.hourly_rate);
             this.dateView = (TextView) itemView.findViewById(R.id.date_text);
             this.timestampView = (TextView) itemView.findViewById(R.id.timeslot_time_text);
             this.locationView = (TextView) itemView.findViewById(R.id.location_text);
